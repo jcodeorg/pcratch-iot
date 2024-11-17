ab43fa0c85ce3a5ec08b868197d3df3a2d39a0c6
Koichi Ito <kito@j-code.org>
2024-11-18 00:39:51 +0900

    BLE通信部分を分離

---
+++ ble_ext.py
@@ -0,0 +1,116 @@
+import asyncio
+import aioble
+import bluetooth
+
+import random
+import struct
+import ubinascii
+import network
+from micropython import const
+
+
+class BLEDevice:
+    def __init__(self):
+        # デバイス名を設定
+        self.wlan = network.WLAN(network.STA_IF)
+        self.wlan.active(True)
+        mac = self.wlan.config('mac')
+        mac_str = ubinascii.hexlify(mac).decode('utf-8').upper()
+        self._NAME = f"BBC micro:bit [{mac_str}]"
+
+        # サービスUUIDと characteristic UUID を定義
+        self.IOT_SERVICE_UUID = bluetooth.UUID('0b50f3e4-607f-4151-9091-7d008d6ffc5c')
+        IOT_COMMAND_CH__UUID = bluetooth.UUID('0b500100-607f-4151-9091-7d008d6ffc5c')
+        IOT_STATE_CH_UUID = bluetooth.UUID('0b500101-607f-4151-9091-7d008d6ffc5c')
+        IOT_MOTION_CH_UUID = bluetooth.UUID('0b500102-607f-4151-9091-7d008d6ffc5c')
+        IOT_PINEVENT_CH_UUID = bluetooth.UUID('0b500110-607f-4151-9091-7d008d6ffc5c')
+        IOT_ACTIONEVENT_CH_UUID = bluetooth.UUID('0b500111-607f-4151-9091-7d008d6ffc5c')
+        IOT_ANALOGIN0_CH_UUID = bluetooth.UUID('0b500120-607f-4151-9091-7d008d6ffc5c')
+        IOT_ANALOGIN1_CH_UUID = bluetooth.UUID('0b500121-607f-4151-9091-7d008d6ffc5c')
+        IOT_ANALOGIN2_CH_UUID = bluetooth.UUID('0b500122-607f-4151-9091-7d008d6ffc5c')
+        IOT_MESSAGE_CH_UUID = bluetooth.UUID('0b500130-607f-4151-9091-7d008d6ffc5c')
+
+        # org.bluetooth.characteristic.gap.appearance.xml
+        self._ADV_APPEARANCE_GENERIC_TAG = const(512)
+        # How frequently to send advertising beacons.
+        self._ADV_INTERVAL_MS = 250_000
+
+        # サービスと characteristic を定義
+        self.iot_service = aioble.Service(self.IOT_SERVICE_UUID)
+        self.command_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_COMMAND_CH__UUID, read=True, write=True
+        )
+        self.state_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_STATE_CH_UUID, read=True
+        )
+        self.motion_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_MOTION_CH_UUID, read=True
+        )
+        self.pinevent_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_PINEVENT_CH_UUID, read=True, notify=True
+        )
+        self.actionevent_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_ACTIONEVENT_CH_UUID, read=True, notify=True
+        )
+        self.analog0_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_ANALOGIN0_CH_UUID, read=True, notify=True
+        )
+        self.analog1_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_ANALOGIN1_CH_UUID, read=True, notify=True
+        )
+        self.analog2_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_ANALOGIN2_CH_UUID, read=True, notify=True
+        )
+        self.message_characteristic = aioble.Characteristic(
+            self.iot_service, IOT_MESSAGE_CH_UUID, read=True, notify=True
+        )
+        aioble.register_services(self.iot_service)
+
+    def state_write(self, buffer):
+        self.state_characteristic.write(buffer, send_update=True)
+
+    # Serially wait for connections. Don't advertise while a central is
+    # connected.
+    async def peripheral_task(self):
+        while True:
+            try:
+                async with await aioble.advertise(
+                    self._ADV_INTERVAL_MS,
+                    name=self._NAME,
+                    services=[self.IOT_SERVICE_UUID],
+                    appearance=self._ADV_APPEARANCE_GENERIC_TAG,
+                ) as connection:
+                    print("Connection from", connection.device)
+                    # 送信する3バイトのデータを定義
+                    data_to_send = struct.pack('<BBB', 0x01, 0x02, 0x03)
+                    self.command_characteristic.write(data_to_send)
+                    print("3バイト送信...") # 3バイトのデータを送信
+                    await connection.disconnected(timeout_ms=None)
+                    print("Disconnected.")
+            except Exception as e:
+                print("Error during advertising or connection:", e)
+                await asyncio.sleep_ms(1000)
+
+
+    async def motion_task(self):
+        while True:
+            #print(i, "motion_characteristic")
+            # 送信する2バイトのデータを9個定義（すべての値がゼロ）
+            data_to_send = struct.pack('<9H', *([0] * 9))
+            self.motion_characteristic.write(data_to_send)
+            await asyncio.sleep_ms(1000)
+
+    async def command_task(self, do_command):
+        while True:
+            # コマンドを待つ
+            await self.command_characteristic.written()
+            data = self.command_characteristic.read()
+            do_command(data)
+
+
+    async def ble_task(self, do_command):
+        # Run both tasks.
+        t2 = asyncio.create_task(self.motion_task())
+        t3 = asyncio.create_task(self.peripheral_task())
+        t4 = asyncio.create_task(self.command_task(do_command))
+        await asyncio.gather(t2, t3, t4)
--- main.py
+++ main.py
@@ -4,74 +4,30 @@ import sys
 
 # ruff: noqa: E402
 sys.path.append("")
-
-from micropython import const
-
-import asyncio
-import aioble
-import bluetooth
-
-import random
-import struct
-import ubinascii
-import network
 from ssd1306 import SSD1306_I2C
 from neopixel import NeoPixel
+from ahtx0 import AHT20
+import asyncio
+from ble_ext import BLEDevice
+import struct
 
-# デバイス名を設定
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-mac = wlan.config('mac')
-mac_str = ubinascii.hexlify(mac).decode('utf-8').upper()
-_NAME = f"BBC micro:bit [{mac_str}]"
 
-# サービスUUIDと characteristic UUID を定義
-_IOT_SERVICE_UUID = bluetooth.UUID('0b50f3e4-607f-4151-9091-7d008d6ffc5c')
-_IOT_COMMAND_CH__UUID = bluetooth.UUID('0b500100-607f-4151-9091-7d008d6ffc5c')
-_IOT_STATE_CH_UUID = bluetooth.UUID('0b500101-607f-4151-9091-7d008d6ffc5c')
-_IOT_MOTION_CH_UUID = bluetooth.UUID('0b500102-607f-4151-9091-7d008d6ffc5c')
-_IOT_PINEVENT_CH_UUID = bluetooth.UUID('0b500110-607f-4151-9091-7d008d6ffc5c')
-_IOT_ACTIONEVENT_CH_UUID = bluetooth.UUID('0b500111-607f-4151-9091-7d008d6ffc5c')
-_IOT_ANALOGIN0_CH_UUID = bluetooth.UUID('0b500120-607f-4151-9091-7d008d6ffc5c')
-_IOT_ANALOGIN1_CH_UUID = bluetooth.UUID('0b500121-607f-4151-9091-7d008d6ffc5c')
-_IOT_ANALOGIN2_CH_UUID = bluetooth.UUID('0b500122-607f-4151-9091-7d008d6ffc5c')
-_IOT_MESSAGE_CH_UUID = bluetooth.UUID('0b500130-607f-4151-9091-7d008d6ffc5c')
+conn = BLEDevice()
 
-# org.bluetooth.characteristic.gap.appearance.xml
-_ADV_APPEARANCE_GENERIC_TAG = const(512)
-# How frequently to send advertising beacons.
-_ADV_INTERVAL_MS = 250_000
-
-# サービスと characteristic を定義
-iot_service = aioble.Service(_IOT_SERVICE_UUID)
-command_characteristic = aioble.Characteristic(
-    iot_service, _IOT_COMMAND_CH__UUID, read=True, write=True
-)
-state_characteristic = aioble.Characteristic(
-    iot_service, _IOT_STATE_CH_UUID, read=True
-)
-motion_characteristic = aioble.Characteristic(
-    iot_service, _IOT_MOTION_CH_UUID, read=True
-)
-pinevent_characteristic = aioble.Characteristic(
-    iot_service, _IOT_PINEVENT_CH_UUID, read=True, notify=True
-)
-actionevent_characteristic = aioble.Characteristic(
-    iot_service, _IOT_ACTIONEVENT_CH_UUID, read=True, notify=True
-)
-analog0_characteristic = aioble.Characteristic(
-    iot_service, _IOT_ANALOGIN0_CH_UUID, read=True, notify=True
-)
-analog1_characteristic = aioble.Characteristic(
-    iot_service, _IOT_ANALOGIN1_CH_UUID, read=True, notify=True
-)
-analog2_characteristic = aioble.Characteristic(
-    iot_service, _IOT_ANALOGIN2_CH_UUID, read=True, notify=True
-)
-message_characteristic = aioble.Characteristic(
-    iot_service, _IOT_MESSAGE_CH_UUID, read=True, notify=True
-)
-aioble.register_services(iot_service)
+# This would be periodically polling a hardware sensor.
+def send_sensor_value():
+    # ピンの値を読み取って、デジタル入力データ (32ビット)のビットフィールドに格納
+    gpio_data = (
+        (pin3.value() << 0) |
+        (pin5.value() << 1) |
+        (pin7.value() << 2)
+    )
+    button_state = 0x00  # ボタンの状態 (24ビット)
+    light_level = int(adc1.read()/4095*255)  # 明るさ (8ビット)
+    temperature = int(adc0.read()/4095*255)  # 温度(0～255)
+    sound_level = 0  # 音レベル (8ビット)
+    buffer = struct.pack('<I3B', gpio_data, light_level, temperature, sound_level)
+    conn.state_write(buffer)
 
 # ESP32C6 Pin layout
 #
@@ -114,20 +70,6 @@ def playtone(frequency, vol):
     sp01.freq(int(frequency))  # Hz
     sp01.duty_u16(int(vol*300))      #
 
-# This would be periodically polling a hardware sensor.
-def send_sensor_value():
-      # ピンの値を読み取って、デジタル入力データ (32ビット)のビットフィールドに格納
-    gpio_data = (
-        (pin3.value() << 0) |
-        (pin5.value() << 1) |
-        (pin7.value() << 2)
-    )
-    button_state = 0x00  # ボタンの状態 (24ビット)
-    light_level = int(adc1.read()/4095*255)  # 明るさ (8ビット)
-    temperature = int(adc0.read()/4095*255)  # 温度(0～255)
-    sound_level = 0  # 音レベル (8ビット)
-    buffer = struct.pack('<I3B', gpio_data, light_level, temperature, sound_level)
-    state_characteristic.write(buffer, send_update=True)
 
 def rgb(r, g, b):
     return (g, r, b)
@@ -168,87 +110,12 @@ pin3.irq(cb03)
 pin5.irq(cb03)
 pin7.irq(cb03)
 
-async def sensor_task():
-    while True:
-        send_sensor_value()
-        await asyncio.sleep_ms(500)
-
-# Serially wait for connections. Don't advertise while a central is
-# connected.
-async def peripheral_task():
-    while True:
-        try:
-            async with await aioble.advertise(
-                _ADV_INTERVAL_MS,
-                name=_NAME,
-                services=[_IOT_SERVICE_UUID],
-                appearance=_ADV_APPEARANCE_GENERIC_TAG,
-            ) as connection:
-                print("Connection from", connection.device)
-                # 送信する3バイトのデータを定義
-                data_to_send = struct.pack('<BBB', 0x01, 0x02, 0x03)
-                command_characteristic.write(data_to_send)
-                print("3バイト送信...") # 3バイトのデータを送信
-                await connection.disconnected(timeout_ms=None)
-                print("Disconnected.")
-        except Exception as e:
-            print("Error during advertising or connection:", e)
-            await asyncio.sleep_ms(1000)
-
-
-async def motion_task():
-    while True:
-        #print(i, "motion_characteristic")
-        # 送信する2バイトのデータを9個定義（すべての値がゼロ）
-        data_to_send = struct.pack('<9H', *([0] * 9))
-        motion_characteristic.write(data_to_send)
-        await asyncio.sleep_ms(1000)
-
-async def command_task():
-    while True:
-        # コマンドを待つ
-        await command_characteristic.written()
-        data = command_characteristic.read()
-        print(data)
-        # Base64 エンコード
-        #binary_data = ubinascii.b2a_base64(data).strip()
-        #print(binary_data)  # 出力: b'\x01Hello'
-        # バイナリデータをリストに変換
-        data_list = list(data)
-        print(data_list)  # 出力: [1, 72, 101, 108, 108, 111]
-        # データを分割して表示
-        #command_id = data_list[0]
-        #command_message = data_list[1:]
-        #print("Command ID:", command_id)  # 出力: Command ID: 1
-        #print("Command Message:", command_message)  # 出力: Command Message: [72, 101, 108, 108, 111]
-        command_id = data_list[0]
-        print(command_id)
-        command_message = data[1:]
-        if command_id == 65:
-            #print("文字", data[2:].decode('utf-8'),"間隔", data[1]*10)
-            display.fill(0)
-            display.text(data[2:].decode('utf-8'), 0, data[1]*10)
-            display.show()
-        elif command_id == 97:
-            # data[2:] から4バイトを取得
-            four_bytes = data[1:5]
-            # 4バイトを uint32 の数値に変換
-            uint32_value = struct.unpack('<I', four_bytes)[0]
-            #print("音", 1000000 / uint32_value, "大きさ", data[5]/255*100)
-            playtone(1000000 / uint32_value, data[5]/255*100)
-        elif command_id == 3:
-            print("Command ID 3: Thank you")
-        elif command_id == 4:
-            print("Command ID 4: Sorry")
-        else:
-            print("Command ID", command_id, "is not defined.")
 
 # 明るさ
 def lux():
     val = adc1.read_u16()/ 65535 * 3.6 * 20/9/10000 * ( 10 ** 6)
     return val
 
-from ahtx0 import AHT20
 aht21 = AHT20(i2c)
 
 async def disp_task():
@@ -275,17 +142,52 @@ async def disp_task():
         display.show()
         await asyncio.sleep_ms(500)
 
+def do_command(data):
+    print(data)
+    # Base64 エンコード
+    #binary_data = ubinascii.b2a_base64(data).strip()
+    #print(binary_data)  # 出力: b'\x01Hello'
+    # バイナリデータをリストに変換
+    data_list = list(data)
+    print(data_list)  # 出力: [1, 72, 101, 108, 108, 111]
+    # データを分割して表示
+    #command_id = data_list[0]
+    #command_message = data_list[1:]
+    #print("Command ID:", command_id)  # 出力: Command ID: 1
+    #print("Command Message:", command_message)  # 出力: Command Message: [72, 101, 108, 108, 111]
+    command_id = data_list[0]
+    print(command_id)
+    command_message = data[1:]
+    if command_id == 65:
+        #print("文字", data[2:].decode('utf-8'),"間隔", data[1]*10)
+        display.fill(0)
+        display.text(data[2:].decode('utf-8'), 0, data[1]*10)
+        display.show()
+    elif command_id == 97:
+        # data[2:] から4バイトを取得
+        four_bytes = data[1:5]
+        # 4バイトを uint32 の数値に変換
+        uint32_value = struct.unpack('<I', four_bytes)[0]
+        #print("音", 1000000 / uint32_value, "大きさ", data[5]/255*100)
+        playtone(1000000 / uint32_value, data[5]/255*100)
+    elif command_id == 3:
+        print("Command ID 3: Thank you")
+    elif command_id == 4:
+        print("Command ID 4: Sorry")
+    else:
+        print("Command ID", command_id, "is not defined.")
 
 
-# Run both tasks.
+async def sensor_task():
+    while True:
+        send_sensor_value()
+        await asyncio.sleep_ms(500)
+
+
 async def main():
-    t1 = asyncio.create_task(sensor_task())
-    t2 = asyncio.create_task(motion_task())
-    t3 = asyncio.create_task(peripheral_task())
-    t4 = asyncio.create_task(command_task())
-    t5 = asyncio.create_task(disp_task())
-    await asyncio.gather(t1, t2, t3, t4, t5)
-
+    t1 = asyncio.create_task(conn.ble_task(do_command))
+    t2 = asyncio.create_task(disp_task())
+    t3 = asyncio.create_task(sensor_task())
+    await asyncio.gather(t1, t2, t3)
 
 asyncio.run(main())
-# ESP32C6 BLE pcratch(micro:bit) v1.0.3
